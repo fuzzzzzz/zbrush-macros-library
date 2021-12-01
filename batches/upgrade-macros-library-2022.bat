925c966d30a12a7b56b336cc37c14611088d2e75
facelessmindz <fuzzzzzz@gmail.com>
2021-12-01 05:06:23 +0100

    add batch file.

---
+++ batches/upgrade-macros-library-2022.bat
@@ -0,0 +1,6 @@
+:: This Batch will copy all the dlls from :
+:: Select the right code line then right click and Run Code.
+:: Code Runner must to be installed
+
+:: Update Python for ZBrush Plugin
+xcopy /s /y "L:\programming\WORKSPACES\\zbrush-macros-library\zbrush-macros-library-2022.txt" "D:\Imaging\Pixologic\ZBrush 2022\ZStartup\ZPlugs64\" /f
